260e23a58b2e04f19e6667b9be50ccc8557b8df0
Vinay846 <Vinayvin84629@gmail.com>
2020-12-10 16:26:41 +0530

    answer

--- src/index.js
+++ src/index.js
@@ -9,6 +9,24 @@ const onePageArticleCount = 10
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+const { newsArticleModel } = require('./connector')
+let offset = 0;
+app.get("/newFeeds", async (req, res)=>{
+    let limit = 0;
+    if(req.query.limit === undefined){
+        limit = 10;
+    }else{
+        limit = parseInt(req.query.limit);
+    }
+    let arr = [];
+    offset += limit;
+    if(await newsArticleModel.countDocuments({}) > offset){
+        arr = await newsArticleModel.find().skip(offset).limit(limit);
+        res.send(arr);
+    }else{
+        res.send([]);
+    }
+})
 
 
 app.listen(port, () => console.log(`App listening on port ${port}!`))
